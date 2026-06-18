15f443ed3836966ddac60e4fd523630e5de2eea3
Aditya Warekar <adityawarekar1984@gmail.com>
2026-06-18 13:33:39 +0530

    complete authentication sysytem with Jwt

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Orders Route Working");
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+console.log(connectDB);
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ecommerce API Running");
+});
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
