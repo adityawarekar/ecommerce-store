052ce776bdacb31319b8549a0fed56c256e48ccf
Aditya Warekar <adityawarekar1984@gmail.com>
2026-06-18 20:17:48 +0530

    Complete ecommerce backend

---
+++ backend/models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,53 @@
 const express = require("express");
 
+const Order = require("../models/Order");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Orders Route Working");
+
+// CREATE ORDER
+router.post("/", async (req, res) => {
+  try {
+    const order = await Order.create(req.body);
+
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+
+// GET ALL ORDERS
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("userId")
+      .populate("products.productId");
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+
+// GET SINGLE ORDER
+router.get("/:id", async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id)
+      .populate("userId")
+      .populate("products.productId");
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
